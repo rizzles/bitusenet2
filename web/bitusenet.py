64de68f4a5642b9855fa6f03bfe292652b7de21f
Mike <reillysemail@gmail.com>
2014-07-07 10:03:09 -0500

    hopefully final fix for vericoin

--- web/bitusenet.py
+++ web/bitusenet.py
@@ -290,7 +290,7 @@ class AddCoinHandler(BaseHandler):
     #@tornado.web.asynchronous
     #@tornado.gen.engine
     def get(self):
-        currency = self.get_argument('currency')
+        currency = self.get_argument('currency', 'btc')
 
         for k,v in self.current_user['addresses'].iteritems():
             if k == currency:
