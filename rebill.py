d352288d21377bf59036efb1fdde3ecf637e58a0
Ubuntu <ubuntu@ip-10-154-189-159.ec2.internal>
2015-04-07 20:14:56 +0000

    couple of changes done on server

--- rebill.py
+++ rebill.py
@@ -3,6 +3,12 @@ import datetime
 import time
 import tornado.httpclient
 import torndb
+import hashlib
+
+
+# md5 of weatherman login for usenet
+m = hashlib.md5()
+password = m.update("zuskejje44")
 
 mongoconn = pymongo.Connection('ec2-50-17-28-132.compute-1.amazonaws.com', 27017)
 mongo = mongoconn.bitusenet
@@ -13,8 +19,37 @@ authdb = torndb.Connection(
 
 users = mongo.users.find()
 
+def deactivate_weatherman(user):
+    print ""
+    print "Making call to weatherman api to deactivate user %s"%user['username']
+    attempts = 0
+    while attempts < 4:
+        attempts += 1
+        try:
+            http_client = tornado.httpclient.HTTPClient()
+            # account group id, auth type 2=user/pass, service class,                                                                                                                                                                                                                                                                       
+            body = "x_username=mikereilly&x_password=%s&x_command=update_customer&x_cust_id=%s&x_acct_status_id=%s"%(m.hexdigest(), user['cust_id'], 3)
+            http_response = http_client.fetch("https://wm.highwinds-media.com/wm/api/http_post.php", method="POST", body=body)
+        except Exception, e:
+            print "Exception deactivating user in weatherman:", e
+            time.sleep(2)
+            continue
+        # 1:x_status, 2:x_error, 3:x_acct_id, 4:x_cust_id                                                                                                                                                                                                                                                                                   
+        print "RESPONSE:", http_response.body
+        resp = http_response.body.split(",")
+        if resp[0] == "0":
+            print "FAILED"
+            return False
+        else:
+            print "SUCCESS"
+            return True
+    return False
+
+
 for user in users:
     if user['billing'] < datetime.datetime.utcnow() and user['active']:
         print user['username'], user['billing'], datetime.datetime.utcnow()
-        #print authdb.execute("DELETE FROM auth.logins WHERE username = %s", user['username'])
-        #mongo.users.update({'username':user['username']},{"$set":{'active':False}})
+        worked = deactivate_weatherman(user)
+        if worked:
+            mongo.users.update({'username':user['username']},{"$set":{'active':False}})
+            
--- web/bitusenet.py
+++ web/bitusenet.py
@@ -21,7 +21,11 @@ import emailer
 from variables import *
 
 clients = {}
-longname = {'btc':'bit', 'ltc':'lite', 'drk':'dark', 'ppc':'peer', 'vtc':'vert', 'doge':'doge', 'max':'max', 'rzr':'razor', 'vrc':'veri'}
+longname = {'btc':'bit', 'ltc':'lite', 'drk':'dark', 'ppc':'peer', 'vtc':'vert', 'doge':'doge', 'max':'max', 'rzr':'razor', 'vrc':'veri', 'via':'via'}
+
+# md5 of weatherman login for usenet
+m = hashlib.md5()
+password = m.update("zuskejje44")
 
 class Application(tornado.web.Application):
     def __init__(self):
@@ -61,7 +65,7 @@ class Application(tornado.web.Application):
             site_name='design',
             xsrf_cookies=True,
             autoescape=None,
-            debug=True,
+            debug=False,
             gzip=True
         )
 
@@ -89,6 +93,53 @@ class BaseHandler(tornado.web.RequestHandler):
     def mongocurrencies(self):
         return self.application.mongocurrencies
 
+    def user_exists(self, username):
+        # Check to see if username already exists
+        exists = self.mongodb.users.find_one({'username': username})
+        if exists:
+            logging.error('username exists on website')
+            return True
+
+        # Check if username exists in auth db.
+        exists = authdb.get("SELECT * FROM auth.logins WHERE username = %s LIMIT 1", username)
+        if exists:
+            logging.error('username exists in auth db.')
+            return True
+
+        logging.info("making a call to weatherman to see if username %s exists"%username)
+        http_client = tornado.httpclient.HTTPClient()
+        body = "x_username=mikereilly&x_password=%s&x_command=username_exists&x_cust_user_id=%s"%(m.hexdigest(), username)
+        http_response = http_client.fetch("https://wm.highwinds-media.com/wm/api/http_post.php", method="POST", body=body)
+        if http_response.body[0] == "1":
+            logging.error(http_response.body)
+            return True
+        # user does not exist. Should be good to go.
+        logging.info(http_response.body)
+        return False
+
+    def change_password_weatherman(self, user, newpassword):
+        logging.info("Making call to weatherman api to change password for user %s"%user['username'])
+        attempts = 0
+        while attempts < 4:
+            attempts += 1
+            try:
+                http_client = tornado.httpclient.HTTPClient()
+                body = """x_username=mikereilly&x_password=%s&x_command=update_customer_password&x_cust_id=%s&x_cust_password=%s&x_cust_new_password=%s"""%(m.hexdigest(), user['cust_id'], user['raw'], newpassword)
+                http_response = http_client.fetch("https://wm.highwinds-media.com/wm/api/http_post.php", method="POST", body=body)
+            except Exception, e:
+                logging.error("Exception adding user to weatherman: %s"%e)
+                time.sleep(1)
+                continue
+            logging.info("RESPONSE: %s"%http_response.body)
+            resp = http_response.body.split(",")
+            if resp[0] == "0":
+                logging.error("FAILED updating password")
+                return False
+            else:
+                logging.info("SUCCESS updating password")
+                return True
+        return False
+
     def get_currencies(self):
         price = self.mongocurrencies.currencies.find_one(sort=[("_id", pymongo.DESCENDING)])
         del(price['_id'])
@@ -101,14 +152,21 @@ class BaseHandler(tornado.web.RequestHandler):
 
     def check_price_age(self):
         for k,v in self.current_user['price'].iteritems():
-            if (datetime.datetime.utcnow() - v[-1]['timestamp']).seconds > 1800:
-                sent = self.current_user['price'][k][-1]['sent']
+            if type(v) is list:
                 currency = self.current_user['price'][k][-1]['currency']
                 price = self.get_currency(currency)
                 logging.info("price is old for user %s. Setting new price for currency %s"%(self.current_user['username'],currency))                
-                newcharge = {'price':price, 'created':int(time.time()), 'currency':currency, 'sent':sent, 'transactions':{}, 'timestamp': datetime.datetime.utcnow(), 'site':'bitusenet' }
-                self.mongodb.users.update({'_id':self.current_user['_id']}, {"$addToSet": {'price.%s'%k: newcharge}})
-                self.get_current_user()
+                newprice = {'price':price, 'created':int(time.time()), 'currency':currency, 'timestamp': datetime.datetime.utcnow(), 'site':'bitusenet' }
+                self.mongodb.users.update({'_id':self.current_user['_id']}, {"$set": {'price.%s'%k: newprice}})
+                self.current_user = self.get_current_user()
+
+            elif (datetime.datetime.utcnow() - v['timestamp']).seconds > 1800:
+                currency = self.current_user['price'][k]['currency']
+                price = self.get_currency(currency)
+                logging.info("price is old for user %s. Setting new price for currency %s"%(self.current_user['username'],currency))                
+                newprice = {'price':price, 'created':int(time.time()), 'currency':currency, 'timestamp': datetime.datetime.utcnow(), 'site':'bitusenet' }
+                self.mongodb.users.update({'_id':self.current_user['_id']}, {"$set": {'price.%s'%k: newprice}})
+                self.current_user = self.get_current_user()
 
 
 class MainHandler(BaseHandler):
@@ -237,10 +295,13 @@ class ActualResetHandler(BaseHandler):
             self.redirect('/reset')
             return
 
+        logging.info("Resetting password to %s"%newpassword)
         salt = uuid.uuid4().hex
         hashed_password = hashlib.sha512(salt + newpassword).hexdigest()
-        self.mongodb.users.update({"_id":user['_id']}, {"$set": {'resetid':None, 'resettime':None, 'salt':salt, 'password':hashed_password, 'raw':newpassword}})
-        authdb.execute("""UPDATE auth.logins SET password = %s WHERE username = %s""", newpassword, user['username'])
+        worked = self.change_password_weatherman(user, newpassword)
+        if worked:
+            self.mongodb.users.update({"_id":user['_id']}, {"$set": {'resetid':None, 'resettime':None, 'salt':salt, 'password':hashed_password, 'raw':newpassword}})
+        #authdb.execute("""UPDATE auth.logins SET password = %s WHERE username = %s""", newpassword, user['username'])
         self.redirect('/login')
 
 
@@ -276,19 +337,8 @@ class SignupHandler(BaseHandler):
         if email:
             email = email.lower()
 
-
-        # Check to see if username already exists
-        exists = self.mongodb.users.find_one({'username': username})
+        exists = self.user_exists(username)
         if exists:
-            logging.error('username exists on website')
-            price = self.get_currencies()
-            self.render('signup.html', errors="usernameexists", aff=aff, uid=uid, price=tornado.escape.json_encode(price), priceobject=price)
-            return
-
-        # Check if username exists in auth db.
-        exists = authdb.get("SELECT * FROM auth.logins WHERE username = %s LIMIT 1", username)
-        if exists:
-            logging.error('username exists in auth db.')
             price = self.get_currencies()
             self.render('signup.html', errors="usernameexists", aff=aff, uid=uid, price=tornado.escape.json_encode(price), priceobject=price)
             return
@@ -332,7 +382,7 @@ class SignupHandler(BaseHandler):
                 'active': False,
                 'addresses': {currency:address},
                 'transactions': {address: []},
-                'price': { address: [{'price':price, 'created':int(time.time()), 'currency':currency, 'sent':0, 'transactions':{}, 'timestamp': datetime.datetime.utcnow(), 'site':'bitusenet' }]},
+                'price': { address: {'price':price, 'created':int(time.time()), 'currency':currency, 'timestamp': datetime.datetime.utcnow(), 'site':'bitusenet' }},
                 'created': datetime.datetime.utcnow(),
                 'billing': datetime.datetime.utcnow(),
                 'aff': aff,
@@ -343,7 +393,8 @@ class SignupHandler(BaseHandler):
         self.set_secure_cookie('bitusenet', username)
         logging.info('Account created for %s with aff of %s'%(username, aff))
         price = self.get_currencies()
-        self.render("success.html", address=address, priceobject=price, price=tornado.escape.json_encode(price), currency=currency, longname=longname)
+        #self.render("success.html", address=address, priceobject=price, price=tornado.escape.json_encode(price), currency=currency, longname=longname)
+        self.redirect("/dashboard-add?currency=%s"%currency)
 
 
 class TransactionReceivedHandler(BaseHandler):
@@ -373,6 +424,15 @@ class WebSocketHandler(tornado.websocket.WebSocketHandler):
 class DashboardHandler(BaseHandler):
     @tornado.web.authenticated
     def get(self):
+        for address, value in self.current_user['price'].iteritems():
+            if isinstance(value, list):
+                currency = value[0]['currency']
+                price = self.get_currency(currency)
+                newprice = {'price':price, 'created':int(time.time()), 'currency':currency, 'timestamp': datetime.datetime.utcnow(), 'site':'bitusenet' }
+                self.mongodb.users.update({'_id':self.current_user['_id']}, {'$set':{'price.%s'%address: newprice, 'addresses.%s'%currency:address}}, True)
+                logging.info("Changed price style for %s"%self.current_user['username'])
+                self.current_user = self.mongodb.users.find_one({'username':self.current_user['username']})
+
         self.render('dashboard.html')
 
 
@@ -420,8 +480,8 @@ class AddCoinHandler(BaseHandler):
         """
 
         price = self.get_currency(currency)
-        newaddress = [{'price':price, 'created':int(time.time()), 'currency':currency, 'sent':0, 'transactions':{}, 'timestamp': datetime.datetime.utcnow(), 'site':'bitusenet' }]
-        self.mongodb.users.update({'_id':self.current_user['_id']}, {'$set':{'price.%s'%address: newaddress, 'addresses.%s'%currency: address, 'transactions.%s'%address: []}}, True)
+        newprice = {'price':price, 'created':int(time.time()), 'currency':currency, 'timestamp': datetime.datetime.utcnow(), 'site':'bitusenet' }
+        self.mongodb.users.update({'_id':self.current_user['_id']}, {'$set':{'price.%s'%address: newprice, 'addresses.%s'%currency: address, 'transactions.%s'%address: []}}, True)
         self.redirect('/dashboard-add?currency=%s'%currency)
 
 
